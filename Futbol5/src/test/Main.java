5e88459d7fad7c8cf71e9852fdb7ee679b87214e
ddsutng8 <herfila2001@yahoo.com.ar>
2014-05-26 23:48:04 -0300

    primer commit

---
+++ Futbol5/src/test/Main.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		jugador pepito=new jugador();
+		pepito.setEdad(30);
+		System.out.println("La edad de pepito es " + pepito.edad);
+
+
+	}
+
+}
---
+++ Futbol5/src/test/partido.java
@@ -0,0 +1,54 @@
+package test;
+import java.util.ArrayList;
+
+public class partido {
+	static private String fecha;
+	static private String lugar;
+	static private int hora;
+	static private int cantidadDeJugadores=0;
+	
+	public String getLugar() {
+		return lugar;
+	}
+public void setLugar(String lugar) {
+	this.lugar = lugar;
+}
+	public String getNombre() {
+		return fecha;
+	}
+	public void setNombre(String nombre) {
+		this.fecha = nombre;
+	}
+	
+	public int getEdad(){
+		return hora;
+	}
+	public void setEdad(int edad) {
+		this.hora = edad;
+	}
+	public int cantidadDeJugadores(){
+		return cantidadDeJugadores;
+	}
+	public void cantidadDeJugadores(int edad) {
+		this.cantidadDeJugadores = edad;
+	}
+	
+	
+	static ArrayList<jugador> ListaDeJugadores = new ArrayList<jugador>();
+
+	public void inscribirA(jugador elJugador) {
+				 {
+			 
+			 partido.ListaDeJugadores.add(cantidadDeJugadores, elJugador) ;
+		//	partido.incribirA jugador;
+		 
+// self.mostrarPorPantallaYobtenerDatos($hora, $fecha, $lugarDelPartido, $persona)
+//	 self.validarSiHayLugarYNoEsCondicional($persona.modo)
+//		 self.listaDeJugadores.Agregar( $persona)}
+
+			}
+
+
+	}
+
+}
